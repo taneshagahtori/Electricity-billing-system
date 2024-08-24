0ad77643a83e19b5b244b3690c1a2f3fc676fb4e
taneshagahtori <iamtanesha13@gmail.com>
2024-08-25 02:31:19 +0530

    img updated

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Splash extends JFrame implements Runnable{ //constructor
     Thread t;
     Splash(){
-        ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("icon/elect.jpg"));
+        ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("icon/img.jpg"));
         Image i2= i1.getImage().getScaledInstance(730, 550, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel image= new JLabel(i3);
